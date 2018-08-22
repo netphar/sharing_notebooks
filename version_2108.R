5f007dc0246c304e707fb444439c9091e3784037
netphar <forfimm2018@gmail.com>
2018-08-22 03:19:14 +0300

    this one reshapes the files. Preserves only groups that result in 3x3 or 3x5 matrix. And calculates synergy

--- version_2108.R
+++ version_2108.R
@@ -16,8 +16,9 @@ rowInsert <- function(x, blockID){
   #  print(class_old)
   NSC2_un <- vector(mode="numeric", length=1)
   NSC2_un <- unique(x$NSC2[!is.na(x$NSC2)])
+
   predicate <- function(x) {
-    if (x[["CONCINDEX2"]]== 0 & x[["NSC1"]] == NSC2_un) {
+    if (as.numeric(x[["CONCINDEX2"]]== 0) & as.numeric(x[["NSC1"]]) == NSC2_un) {
       return(T)
     }
     else {return(F)}
@@ -117,7 +118,8 @@ correctingConc <- function(x,y,z) {
 datalist_full <- list()
 
 #cellnames to be searched for
-cellnames <- unique(nov2017$CELLNAME)
+cellnames_old <- unique(nov2017$CELLNAME)
+cellnames <- cellnames_old[-61]
 
 #cellnames <- c("HCT-116")
 
@@ -159,6 +161,359 @@ for (name in cellnames) {
   
 }
 
+#not sure what s the better way or rather if there is any difference between thes two. Choose either. resuts and runtime are identical
+# one
+do.call(rbind, datalist_full) -> one
+do.call(rbind, one) -> two
+two_cleaned <- two %>% group_by(BlockID) %>% mutate(nrows = n()) %>% filter(nrows == 16 | nrows == 24)
+# two
+do.call(rbind, do.call(rbind, datalist_full)) -> unbound
+unbound_filtered <- unbound %>%
+  group_by(BlockID) %>%
+    mutate(nrows = n()) %>%
+      filter(nrows == 16 | nrows == 24)
+
+#reshaping
+reshaped <- ReshapeData(unbound_filtered, data.type = 'viability')
+
+
+# for synergy calc
+Loewe_modded <- function (response.mat, correction = T, Emin = 0, Emax = 100, 
+                          nan.handle = c("LL4", "L4")) 
+{
+  if (correction) {
+    response.mat <- BaselineCorrectionSD(response.mat, Emin = Emin, 
+                                         Emax = Emax, nan.handle)$corrected.mat
+  }
+  single.fit <- FittingSingleDrug(response.mat)
+  drug.col.model <- single.fit$drug.col.model
+  drug.col.par <- coef(drug.col.model)
+  d1.fun <- function(conc, drug.col.par) {
+    (drug.col.par[3] + drug.col.par[2] * (conc/drug.col.par[4])^drug.col.par[1])/(1 + 
+                                                                                    (conc/drug.col.par[4])^drug.col.par[1])
+  }
+  drug.row.model <- single.fit$drug.row.model
+  drug.row.par <- coef(drug.row.model)
+  d2.fun <- function(conc, drug.row.par) {
+    (drug.row.par[3] + drug.row.par[2] * (conc/drug.row.par[4])^drug.row.par[1])/(1 + 
+                                                                                    (conc/drug.row.par[4])^drug.row.par[1])
+  }
+  row.conc <- as.numeric(rownames(response.mat))[-1]
+  col.conc <- as.numeric(colnames(response.mat))[-1]
+  loewe.mat <- response.mat
+  for (i in 1:length(col.conc)) {
+    for (j in 1:length(row.conc)) {
+      x1 <- col.conc[i]
+      x2 <- row.conc[j]
+      eq <- function(x) {
+        x1/(drug.col.par[4] * (((x - drug.col.par[3])/(drug.col.par[2] - 
+                                                         x))^(1/drug.col.par[1]))) + x2/(drug.row.par[4] * 
+                                                                                           (((x - drug.row.par[3])/(drug.row.par[2] - 
+                                                                                                                      x))^(1/drug.row.par[1]))) - 1
+      }
+      slv <- nleqslv(max(drug.col.par[2] + 1, drug.row.par[2] + 
+                           1), eq, method = "Newton")
+      if (slv$termcd == 1) {
+        loewe.mat[j + 1, i + 1] <- slv$x
+      }
+      else {
+        y.loewe1 <- d1.fun(x1 + x2, drug.col.par)
+        y.loewe2 <- d2.fun(x1 + x2, drug.row.par)
+        loewe.mat[j + 1, i + 1] <- ifelse(y.loewe1 > 
+                                            y.loewe2, y.loewe1, y.loewe2)
+      }
+    }
+  }
+  return(response.mat - loewe.mat)
+}
+Loewe <- function (response.mat,correction = T, Emin = 0, Emax = 100,nan.handle = c("LL4", "L4")) {
+  # scores <- list()
+  #  method <- "Loewe"
+  out <- tryCatch(Loewe_modded(response.mat,correction = correction, Emin = Emin, Emax = Emax, 
+                               nan.handle = nan.handle), error = function(e) NA)
+  #  {
+  #  x$method <- method
+  #  x$scores <- x$dose.response.mats
+  #  x$scores[!is.na(x$scores)] <- NA
+  #  x$scores <- replace(x$scores, !is.na(x$scores), NA)
+  #  purrr::map(!is.na(x$scores), NA)
+  #  x$scores <- lapply(x$scores, function(z) ifelse(!is.na(z), NA, z))
+  #  return(x)
+  #  })
+  return(out)
+}
+
+ZIP_modded <- function (response.mat, correction = T, Emin = 0, Emax = 100, 
+                        nan.handle = c("LL4", "L4")) 
+{
+  if (correction) {
+    nan.handle <- match.arg(nan.handle)
+    response.mat <- BaselineCorrectionSD(response.mat, NA, 
+                                         NA, nan.handle)$corrected.mat
+  }
+  single.fitted <- FittingSingleDrug(response.mat, fixed = c(NA, 
+                                                             Emin, Emax, NA), nan.handle)
+  drug.col.response <- single.fitted$drug.col.fitted
+  drug.row.response <- single.fitted$drug.row.fitted
+  updated.single.mat <- mat.or.vec(nrow(response.mat), ncol(response.mat))
+  colnames(updated.single.mat) <- colnames(response.mat)
+  rownames(updated.single.mat) <- rownames(response.mat)
+  updated.single.mat[1, c(2:ncol(response.mat))] <- drug.col.response
+  updated.single.mat[c(2:nrow(response.mat)), 1] <- drug.row.response
+  updated.col.mat <- updated.single.mat
+  for (i in 2:ncol(response.mat)) {
+    tmp <- as.data.frame(mat.or.vec(nrow(response.mat) - 
+                                      1, 0))
+    tmp$dose <- as.numeric(rownames(response.mat)[-1])
+    tmp$inhibition <- response.mat[c(2:nrow(response.mat)), 
+                                   i]
+    tmp.min <- updated.single.mat[1, i]
+    if (var(tmp$inhibition, na.rm = TRUE) == 0) {
+      tmp$inhibition[1] <- tmp$inhibition[1] - 10^-10
+    }
+    tmp.model <- drm(inhibition ~ dose, data = tmp, fct = L.4(fixed = c(NA, 
+                                                                        tmp.min, Emax, NA)), na.action = na.omit)
+    tmp$fitted.inhibition <- suppressWarnings(fitted(tmp.model))
+    if (tmp$fitted.inhibition[nrow(response.mat) - 1] < 0) 
+      tmp$fitted.inhibition[nrow(response.mat) - 1] <- tmp.min
+    updated.col.mat[c(2:nrow(response.mat)), i] <- tmp$fitted.inhibition
+  }
+  updated.row.mat <- updated.single.mat
+  for (i in 2:nrow(response.mat)) {
+    tmp <- as.data.frame(mat.or.vec(ncol(response.mat) - 
+                                      1, 0))
+    tmp$dose <- as.numeric(colnames(response.mat)[-1])
+    tmp$inhibition <- response.mat[i, c(2:ncol(response.mat))]
+    tmp.min <- updated.single.mat[i, 1]
+    if (var(tmp$inhibition, na.rm = TRUE) == 0) {
+      tmp$inhibition[1] <- tmp$inhibition[1] - 10^-10
+    }
+    tmp.model <- drm(inhibition ~ dose, data = tmp, fct = L.4(fixed = c(NA, 
+                                                                        tmp.min, Emax, NA)), na.action = na.omit)
+    tmp$fitted.inhibition <- suppressWarnings(fitted(tmp.model))
+    if (tmp$fitted.inhibition[ncol(response.mat) - 1] < 0) 
+      tmp$fitted.inhibition[ncol(response.mat) - 1] <- tmp.min
+    updated.row.mat[i, c(2:ncol(response.mat))] <- tmp$fitted.inhibition
+  }
+  fitted.mat <- (updated.col.mat + updated.row.mat)/2
+  zip.mat <- updated.single.mat
+  for (i in 2:nrow(updated.single.mat)) {
+    for (j in 2:ncol((updated.single.mat))) {
+      zip.mat[i, j] <- updated.single.mat[i, 1] + updated.single.mat[1, 
+                                                                     j] - updated.single.mat[i, 1] * updated.single.mat[1, 
+                                                                                                                        j]/100
+    }
+  }
+  fitted.mat[1, 1] <- 0
+  zip.mat[1, 1] <- 0
+  fitted.mat <- apply(fitted.mat, c(1, 2), function(x) ifelse(x > 
+                                                                100, 100, x))
+  delta.mat <- (fitted.mat - zip.mat)
+  delta.mat
+}
+
+ZIP <- function (response.mat,correction = T, Emin = 0, Emax = 100,nan.handle = c("LL4", "L4")) {
+  # scores <- list()
+  #  method <- "Loewe"
+  out <- tryCatch(ZIP_modded(response.mat,correction = correction, Emin = Emin, Emax = Emax, 
+                             nan.handle = nan.handle), error = function(e) NA)
+  #  {
+  #  x$method <- method
+  #  x$scores <- x$dose.response.mats
+  #  x$scores[!is.na(x$scores)] <- NA
+  #  x$scores <- replace(x$scores, !is.na(x$scores), NA)
+  #  purrr::map(!is.na(x$scores), NA)
+  #  x$scores <- lapply(x$scores, function(z) ifelse(!is.na(z), NA, z))
+  #  return(x)
+  #  })
+  return(out)
+}
+
+BLISS_modded<- function (response.mat, correction = T, Emin = 0, Emax = 100, 
+                         nan.handle = c("LL4", "L4")) 
+{
+  if (correction) {
+    response.mat <- BaselineCorrectionSD(response.mat, Emin = Emin, 
+                                         Emax = Emax, nan.handle)$corrected.mat
+  }
+  drug1.response <- response.mat[, 1]
+  drug2.response <- response.mat[1, ]
+  ref.mat <- response.mat
+  for (i in 2:nrow(response.mat)) {
+    for (j in 2:ncol(response.mat)) {
+      ref.mat[i, j] <- drug1.response[i] + drug2.response[j] - 
+        drug1.response[i] * drug2.response[j]/100
+    }
+  }
+  syn.mat <- response.mat - ref.mat
+  syn.mat
+}
+
+Bliss <- function (response.mat,correction =T, Emin = 0, Emax = 100,nan.handle = c("LL4", "L4")) {
+  # scores <- list()
+  #  method <- "ZIP"
+  out <- tryCatch(BLISS_modded(response.mat,correction = correction, Emin = Emin, Emax = Emax, 
+                               nan.handle = nan.handle), error = function(e) NA)
+  #  {
+  #  x$method <- method
+  #  x$scores <- x$dose.response.mats
+  #  x$scores[!is.na(x$scores)] <- NA
+  #  x$scores <- replace(x$scores, !is.na(x$scores), NA)
+  #  purrr::map(!is.na(x$scores), NA)
+  #  x$scores <- lapply(x$scores, function(z) ifelse(!is.na(z), NA, z))
+  #  return(x)
+  #  })
+  return(out)
+}
+
+HSA <- function (response.mat,correction = T, Emin = 0, Emax = 100,nan.handle = c("LL4", "L4")) {
+  # scores <- list()
+  #  method <- "ZIP"
+  out <- tryCatch(HSA_modded(response.mat,correction = correction, Emin = Emin, Emax = Emax, 
+                             nan.handle = nan.handle), error = function(e) NA)
+  #  {
+  #  x$method <- method
+  #  x$scores <- x$dose.response.mats
+  #  x$scores[!is.na(x$scores)] <- NA
+  #  x$scores <- replace(x$scores, !is.na(x$scores), NA)
+  #  purrr::map(!is.na(x$scores), NA)
+  #  x$scores <- lapply(x$scores, function(z) ifelse(!is.na(z), NA, z))
+  #  return(x)
+  #  })
+  return(out)
+}
+
+HSA_modded <-function (response.mat, correction = T, Emin = 0, Emax = 100, 
+                       nan.handle = c("LL4", "L4")) 
+{
+  if (correction) {
+    response.mat <- BaselineCorrectionSD(response.mat, Emin = Emin, 
+                                         Emax = Emax, nan.handle)$corrected.mat
+  }
+  drug1.response <- response.mat[, 1]
+  drug2.response <- response.mat[1, ]
+  ref.mat <- response.mat
+  for (i in 2:nrow(response.mat)) {
+    for (j in 2:ncol(response.mat)) {
+      ref.mat[i, j] <- ifelse(drug1.response[i] > drug2.response[j], 
+                              drug1.response[i], drug2.response[j])
+    }
+  }
+  syn.mat <- response.mat - ref.mat
+  syn.mat
+}
+
+#####
+#some error handling
+# this throws the dim error
+#  3053  750690 in cell line SK-MEL-28
+test <- for_sorting %>%
+  filter(CELLNAME == 'SK-MEL-28')
+x <- test
+y <- 256942
+z <- 256439
+
+
+y <- as.numeric(y)
+z <- as.numeric(z)
+
+x <- filter(x, ((NSC1 == y & NSC2 == z) | (NSC1 == y & is.na(NSC2)) | (NSC1 == z & is.na(NSC2)) ))
+
+x_comb <- list()
+x_comb <- filter(x, ( (NSC1 == y & NSC2 == z) ) )
+x_comb <- x_comb %>%
+  group_by(NSC1, NSC2, CONC1, CONC2) %>%
+  mutate(mean_single = mean(PERCENTGROWTH)) %>% # this gets mean values for all the experiments where one drug is tested by itself in cell line == 786-0
+  arrange()
+
+
+aCONC11 <- unique(filter(x_comb, (!is.na(CONC2))  )$CONC1)
+aNSC11 <- y
+aCONC21 <- unique(filter(x_comb, (!is.na(CONC2))  )$CONC2)
+aNSC21 <-z
+
+x_one <- filter(x, (CONC1 %in% aCONC11)& NSC1 ==aNSC11 & is.na(CONC2))
+x_one <- x_one %>%
+  group_by(CONC1) %>%
+  mutate(mean_single = mean(PERCENTGROWTH)) %>% # this gets mean values for all the experiments where one drug is tested by itself in cell line == 786-0
+  arrange()
+df <- data.frame()
+df <- as.data.frame(cbind(order(aCONC11), aCONC11))
+x_one <- inner_join(x_one, df, by=c("CONCINDEX1" = "V1", "CONC1" = "aCONC11"))
+x_one <- x_one[!duplicated(x_one[c("CONC1")]),]
+
+x_two <- filter(x, (CONC1 %in% aCONC21)& NSC1 ==aNSC21 & is.na(CONC2))
+x_two <- x_two %>%
+  group_by(CONC1) %>%
+  mutate(mean_single = mean(PERCENTGROWTH)) %>% # this gets mean values for all the experiments where one drug is tested by itself in cell line == 786-0
+  arrange()
+df2 <- data.frame()
+df2 <- as.data.frame(cbind(order(aCONC21), aCONC21))
+x_two <- inner_join(x_two, df2, by=c("CONCINDEX1" = "V1", "CONC1" = "aCONC21"))
+x_two <- x_two[!duplicated(x_two[c("CONC1")]),]
+
+x_comb <- rbind(x_comb, x_one)
+x_comb <- rbind(x_comb, x_two)
+
+x <- x_comb
+
+class_old <- list()
+class_old <- sapply(x, class)
+#  print(class_old)
+NSC2_un <- vector(mode="numeric", length=1)
+NSC2_un <- unique(x$NSC2[!is.na(x$NSC2)])
+predicate <- function(x) {
+  if (as.numeric(x[["CONCINDEX2"]]== 0) & as.numeric(x[["NSC1"]]) == NSC2_un) {
+    return(T)
+  }
+  else {return(F)}
+}
+exchange <- function(x) {
+  if (predicate(x)){
+    placeholderIndex <- x[["CONCINDEX1"]]
+    placeholderConc <- x[["CONC1"]]
+    x[["CONCINDEX1"]] <- x[["CONCINDEX2"]]
+    x[["CONCINDEX2"]] <- placeholderIndex
+    x[["CONC1"]] <- x[["CONC2"]]
+    x[["CONC2"]] <- placeholderConc
+    return(x)
+  } else { return(x)}
+} 
+
+x <- as.tibble(t(apply(x, 1, exchange)))
+x[] <- Map(`class<-`,x, class_old) #brilliant! https://stackoverflow.com/questions/27435873/changing-class-of-data-frame-columns-using-strings
+#  class_new <- sapply(x, class)
+#  print(class_new)
+
+
+
+
+NSC1 <- dplyr::setdiff(unique(x$NSC1[!is.na(x$NSC1)]),NSC2_un)
+#  NSC1 <- unique(x$NSC1[!is.na(x$NSC1)])
+x[(nrow(x)+1),] <- x[nrow(x),]
+x[nrow(x),]$NSC1 <- NSC1
+x[nrow(x),]$NSC2 <- NSC2_un
+x[nrow(x),]$CONCINDEX1 <- x[nrow(x),]$CONCINDEX2 <- x[nrow(x),]$CONC1 <- x[nrow(x),]$CONC2 <- 0
+x[nrow(x),]$mean_single <- 100
+x[nrow(x),]$SAMPLE2 <- unique(x$SAMPLE2[!is.na(x$SAMPLE2)])
+
+x$Row <- x$CONCINDEX2 + 1
+x$Col <- x$CONCINDEX1 + 1
+x$BlockID <- c(blockID) # sub with cell-line
+x$Response <- x$mean_single
+x$ConcRowUnit <- x$ConcColUnit <- c("M")
+x$ConcRow <- x$CONC2
+x$ConcCol <- x$CONC1
+x$ConcCol[is.na(x$ConcCol) ] <- 0
+x$DrugRow <- NSC2_un
+x$DrugCol <- NSC1
+x[is.na(x$ConcRow),]$ConcRow <- 0
+#  x <- x[,-c(1:16)] # to-do change that
+x <- x[-match(c("COMBODRUGSEQ","SCREENER","STUDY", "TESTDATE", "PLATE", "PANELNBR", "CELLNBR", "PREFIX1", "NSC1", "SAMPLE1", "CONCINDEX1", "CONC1", "CONCUNIT1", "PREFIX2", "NSC2", "SAMPLE2", "CONCINDEX2", "CONC2", "CONCUNIT2", "PERCENTGROWTH", "PERCENTGROWTHNOTZ","TESTVALUE", "CONTROLVALUE", "TZVALUE", "EXPECTEDGROWTH", "SCORE", "VALID", "PANEL", "CELLNAME", "mean_single"), names(x))]
+return(x)
+
+
+#####
 
 do.call(rbind, datalist_full) -> trying
 do.call(rbind, trying) -> trying
