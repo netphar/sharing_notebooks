511cf97a61af8b2d3fcc811a436310b8d2b3f461
Zagidullin <zagidull@LM4-tike-minni.local>
2018-10-01 20:41:02 +0300

    adding progress bar and a script to reshape bliss

---
+++ cleanup/0110_bliss.R
@@ -0,0 +1,21 @@
+#housekeeping
+rm(list=ls(all=TRUE))
+
+setwd('/home/bulat/NCI/cleanup')
+
+#required libraries
+library("dplyr")
+library('drc')
+library('nleqslv')
+library('readr')
+library('reshape2')
+library('tibble')
+
+source('ReshapeForDB.R')
+file <- '2609_reshaped.Bliss'
+reshaped <- readRDS(file)
+
+to.bind <- ReshapeForDB(reshaped)
+calculated.synergy.rbound.datalist = do.call(rbind, to.bind)
+
+saveRDS(object = calculated.synergy.rbound.datalist, file = '0110_rbound.Bliss')
--- cleanup/ReshapeForDB.R
+++ cleanup/ReshapeForDB.R
@@ -4,36 +4,42 @@ ReshapeForDB = function(x) {
   to.populate <- which(is.na(x$scores))
   calculated.synergy.datalist <- vector('list', length(iter))
   
+  temp.pb <- txtProgressBar(min = 0, max = length(iter), style = 3)
+  
   for (i in iter) {
+    
+    # progress bar
+    setTxtProgressBar(temp.pb, i)
     
     # precalc values of the drug.pairs. Useful if we need to add drug.row and drug.col. Otherwise probably unnecessary
     temp.info <- x$drug.pairs[i,]
     
     # get dose-response values in long
-    temp.dose <- melt(x$dose.response.mats[[i]])
+    temp.dose <- reshape2::melt(x$dose.response.mats[[i]])
     
     # get synergy scores in long
     # if score is NA, reconstruct a matrix with the same dimension as dose.response.mats filled with NA's
     if (i %in% to.populate) {
-      temp.scores <- melt(matrix(, nrow = nrow(x$dose.response.mats[[i]]), ncol = ncol(x$dose.response.mats[[i]]), 
+      temp.scores <- reshape2::melt(matrix(, nrow = nrow(x$dose.response.mats[[i]]), ncol = ncol(x$dose.response.mats[[i]]), 
                                  dimnames = dimnames(x$dose.response.mats[[i]])))
     } else {
-      temp.scores <- melt(x$scores[[i]])
+      temp.scores <- reshape2::melt(x$scores[[i]])
     }
     
     # merge both df's together
-    temp <- merge(temp.dose,temp.scores, by = c('Var1', 'Var2'))
+    temp <- base::merge(temp.dose,temp.scores, by = c('Var1', 'Var2'))
     
     # computer number of rows in long format of data in order to know how many times we need to paste the blockID
-    times <- nrow(temp)
+    nrows <- nrow(temp)
     
     # adjust names and add drug row/col and blockID's
     names(temp) <- c('ConcR','ConcC','ResponseInhibition', 'SynergyHSA')
-    temp$BlockID <- rep(temp.info$blockIDs, times = times)
+    temp$BlockID <- rep(temp.info$blockIDs, times = nrows)
     #    temp$DrugRow <- rep(temp.info$drug.row, times = times)
     #    temp$DrugCol <- rep(temp.info$drug.col, times = times)
     calculated.synergy.datalist[[i]] <- temp
     
   }
+  close(temp.pb)
   return(calculated.synergy.datalist)
   }
